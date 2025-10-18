bd6ce69dae925e76ec7bb313dcc28dd97e98a8d1
mark roby <72339498+markroby@users.noreply.github.com>
2025-10-18 21:57:15 +0300

    Delete api directory

--- api/index.js
+++
@@ -1,5 +0,0 @@
-// api/index.js
-// Vercel serverless entry
-const serverless = require('serverless-http');
-const app = require('../server');  // uses module.exports from server.js
-module.exports = (req, res) => serverless(app)(req, res);
